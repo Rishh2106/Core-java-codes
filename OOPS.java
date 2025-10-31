dadd58705675b730d45797023943e55dd5f7a40d
Rishh2106 <rishijaon05@gmail.com>
2025-10-31 13:22:04 +0530

    Abstraction and encapsulation done

--- OOPS.java
+++ OOPS.java
@@ -1,15 +1,30 @@
-public class OOPS {
-    private class ExampleClass1 {
-        String name="From class";
-        int order=1;
-        public void display() {
-            System.out.println("Name: " + name + order);
+
+public class OOPS {                                                       //Class
+    abstract class OrderProcessor {
+    // Abstract method: the implementation details are hidden.
+    abstract void updateOrder(int orderUpdate);
+    abstract void display();
+}
+    private class ExampleClass1 extends OrderProcessor {                 // Inheritance from abstract class
+        String name = "From class";                                      // Encapsulation of data
+        private int order = 1;
+
+        @Override
+        public void updateOrder(int orderUpdate) {
+            if (orderUpdate > 50) {
+                this.order = orderUpdate;
+            }
         }
-        //Example of class and encapsulation
-        
+
+        @Override
+        public void display() {
+            System.out.println("Name: " + name + ", Order: " + order);
+        }
     }
+
     public static void main(String[] args) {
-        ExampleClass1 obj = new OOPS().new ExampleClass1(); //Creating object of inner private class
-        obj.display();
+        ExampleClass1 obj = new OOPS().new ExampleClass1();                   //Object
+        obj.updateOrder(100); // Using the abstraction method to update the order
+        obj.display(); // Using the abstraction method to display the order
     }
 }
